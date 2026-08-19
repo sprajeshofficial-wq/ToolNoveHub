3dd5a12228bb400fdf43fdfaa8eb4f75e0da2b21
srajesh1330 <sprajeshofficial@gmail.com>
2026-08-20 02:40:59 +0530

    📝 Add sitemap and robots.txt

--- app/robots.ts
+++ app/robots.ts
@@ -2,10 +2,15 @@ import type { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+
     sitemap: 'https://toolnovehub.tools/sitemap.xml',
+
+    host: 'https://toolnovehub.tools',
   };
 }
